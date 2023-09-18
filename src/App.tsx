0a1a6d1fff74464ffb24752783cc4e0444543396
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-09-18 19:35:13 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App() {
   const [model, setModel] = useState<speech.SpeechCommandRecognizer | null>(
     null
   );
-
-  const [prediction, setPrediction] = useState<string>("");
   const [labels, setLabels] = useState<string[]>([]);
-
   const [ready, setReady] = useState(false);
+  const [prediction, setPrediction] = useState<string>("");
+  const [isListening, setIsListening] = useState(false);
+
   useEffect(() => {
     load_model()
       .then(({ model, labels }) => {
@@ -20,29 +20,55 @@ function App() {
       .catch((err) => console.log(err));
   }, []);
 
-  const recognizeCommands = async () => {
+  function handleStart() {
     if (!model) return;
-    console.log("Listening for commands");
+    setIsListening(true);
     model.listen(
       async (result) => {
-        console.log(result.spectrogram);
-        console.log(result.scores);
-        const scores = result.scores;
-        const prediction = labels[argMax(Object.values(scores))];
+        console.log(result);
+        const scores = result.scores; // Get the scores from the model output.
+        const scoresValues = Object.values(scores); // Get the scores as an array
+        const idxMax = argMax(scoresValues); // Get the index of the highest score
+        const prediction = labels[idxMax]; // Get the label of the highest score
         setPrediction(prediction);
       },
-      { includeSpectrogram: true, probabilityThreshold: 0.9 }
+      { includeSpectrogram: false, probabilityThreshold: 0.8 } // Can be tuned for different voices.
     );
-    setTimeout(() => model.stopListening(), 10e3);
-  };
+    setTimeout(() => {
+      model.stopListening();
+      setIsListening(false);
+    }, 60e3); // Stop listening after 60 seconds.
+  }
+
+  function handleStop() {
+    if (!model) return;
+    model.stopListening();
+    setIsListening(false);
+  }
 
   if (!ready) return <div>...loading</div>;
 
-  return <></>;
+  return (
+    <>
+      <h1>Speech Recognition</h1>
+      <div>
+        <button onClick={handleStart} disabled={isListening}>
+          Start
+        </button>
+        <button onClick={handleStop} disabled={!isListening}>
+          Stop
+        </button>
+        <div>
+          {prediction ? <h3>{prediction}</h3> : <h3>No Voice Detected</h3>}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
 
-function argMax(arr: number[]) {
-  return arr.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+// Retrieve the array key corresponding to the largest element in the array.
+function argMax(array: number[]) {
+  return array.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
 }
--- src/model.ts
+++ src/model.ts
@@ -3,8 +3,8 @@ import * as speech from "@tensorflow-models/speech-commands";
 export async function load_model() {
   try {
     const recognizer = speech.create("BROWSER_FFT");
-    const labels = recognizer.wordLabels();
     await recognizer.ensureModelLoaded();
+    const labels = recognizer.wordLabels();
     return { model: recognizer, labels };
   } catch (err) {
     console.log(err);
